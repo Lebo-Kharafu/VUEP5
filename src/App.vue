47363e620c12515bc323b1c6442b08a9501cc5b3
Lebo-Kharafu <Kharafu23@gmail.com>
2025-08-20 21:22:54 +0200

    Refactor:  App.vue added: screen.vue
    reshuffled the App to be more component like.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup>
   import { ref } from 'vue';
-  import gameCard from './components/gameCard.vue';
-  import BouncingBall from "./views/BouncingBall.vue";
-  import CatchTheBall from "./views/CatchTheBall.vue";
-  import Snake from "./views/snake.vue";
+  import screen from './views/screen.vue';
 
-  let gameTitle = ref('Snake');
 </script>
 
 <template>
@@ -13,22 +9,7 @@
     <header id="header">
       <h1>Welcome to the Game Gallery: Vite + Vue + P5.js</h1>
     </header>
-    <main id="main-grid">
-      <aside id="menu">
-        <gameCard title="Bouncing ball" description="This is a simple Bouncing ball"  @click="gameTitle = 'Bounce-Ball'"/>
-        <gameCard title="Snake" description="This is a simple snake game"  @click="gameTitle = 'Snake'"/>
-        <gameCard title="Catch The Ball" description="This is a simple Catching The Ball game" @click="gameTitle = 'Catch-Ball'" />
-      </aside>
-      <section id="game-viewer">
-        <h3>Now Playing {{ gameTitle }}</h3>
-        <CatchTheBall  v-if="gameTitle === 'Catch-Ball'" wWIDTH="600" wHEIGHT="400" />
-        <BouncingBall  v-else-if="gameTitle === 'Bounce-Ball'" wWIDTH="600" wHEIGHT="400" />
-        <Snake  v-else-if="gameTitle === 'Snake'" wWIDTH="600" wHEIGHT="400" />
-      </section>
-      <section id="controls">
-        <button @click="gameTitle = 'NO Game'">STOP GAMES</button>
-      </section>
-    </main>
+    <screen/>    
     <footer id="footer">
       <p>Since 2025</p>
     </footer>
@@ -36,49 +17,17 @@
 </template>
 
 <style scoped>
-#global-container {
+ #global-container {
   display: flex;
   flex-direction: column;
   width: 100vw;
-  height: 100%;
 }
 
 #header {
   background-color: aquamarine;
 }
 
-#main-grid {
-  /* background-color: blueviolet; */
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-template-rows: 2fr auto;
-  grid-template-areas: 
-    'menu game-viewer'
-    'menu controls';
-    flex-grow: 1;
-}
 
-#menu {
-  background-color: black;
-  grid-area: menu;
-  max-width: 300px;
-  max-height: 515px;
-  overflow-y: auto;
-}
-
-#game-viewer {
-  /* background-color: chocolate; */
-  grid-area: game-viewer;
-
-}
-
-#controls {
-  background-color: darkslategray;
-  grid-area: controls;
-
-}
-
-#footer {
-  /* background-color: firebrick; */
-}
+/* #footer {
+} */
 </style>
---
+++ src/views/screen.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue';
+import gameCard from '../components/gameCard.vue';
+import BouncingBall from "./BouncingBall.vue";
+import CatchTheBall from "./CatchTheBall.vue";
+import Snake from "./snake.vue";
+let gameTitle = ref('Snake');
+let wsize = 600;
+let hsize = 400;
+</script>
+
+<template>
+    <section id="main-grid">
+        <aside id="side-bar">
+            <gameCard title="Bouncing ball" description="This is a simple Bouncing ball"
+                @click="gameTitle = 'Bounce-Ball'" />
+            <gameCard title="Snake" description="This is a simple snake game" @click="gameTitle = 'Snake'" />
+            <gameCard title="Catch The Ball" description="This is a simple Catching The Ball game"
+                @click="gameTitle = 'Catch-Ball'" />
+        </aside>
+        <section id="frame">
+            <h3>Now Playing {{ gameTitle }}</h3>
+            <CatchTheBall v-if="gameTitle === 'Catch-Ball'" wWIDTH="size" wHEIGHT="400" />
+            <BouncingBall v-else-if="gameTitle === 'Bounce-Ball'" wWIDTH="600" wHEIGHT="400" />
+            <Snake v-else-if="gameTitle === 'Snake'" wWIDTH="600" wHEIGHT="400" />
+        </section>
+        <section id="controls">
+            <button @click="gameTitle = 'NO Game'">STOP GAMES</button>
+        </section>
+    </section>
+</template>
+
+<style scoped>
+#main-grid {
+    display: grid;
+    grid-template-areas: "side-bar frame"
+        "side-bar frame"
+        "side-bar controls";
+    /* grid-template-columns: 1fr 4fr;
+    grid-template-rows: 2fr 2fr 1fr; */
+}
+
+#side-bar {
+    grid-area: side-bar;
+    max-width: 20rem;
+    height: fit-content;
+}
+
+#frame {
+    grid-area: frame;
+}
+
+#controls {
+    grid-area: controls;
+
+}
+</style>
